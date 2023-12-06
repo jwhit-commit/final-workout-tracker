4b76dad92546e428b212b1138f5eea7a21dff062
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-12-06 09:21:07 -0600

    adding shop backend files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -45,3 +45,42 @@ export const QUERY_WORKOUTS = gql`
         }
     }
 `;
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ProductInput]) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      category {
+        name
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,7 @@ type User {
     email: String
     password: String
     workouts: [Workout]
+    orders: [Order]
 }
 
 type Workout {
@@ -25,14 +26,46 @@ input Exercise {
     target: String
 }
 
+type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+  }
+
+type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
 type Auth {
     token: ID!
     user: User
 }
 
+input ProductInput {
+    _id: ID
+    purchaseQuantity: Int
+    name: String
+    image: String
+    price: Float
+    quantity: Int
+  }
+
+type Checkout {
+    session: ID
+  }
+
 type Query {
     users: [User]
     user(username: String!): User
+    products(name: String): [Product]
+    product(_id: ID!): Product
+    order(_id: ID!): Order
+    checkout(products: [ProductInput]): Checkout
 }
 
 type Mutation {
@@ -40,6 +73,8 @@ type Mutation {
     login(email: String!, password: String!): Auth
     addWorkout(bodyPart: String!, exercises: [Exercise]): Workout
     updateWorkout(_id: ID!, exercises: [Exercise]): Workout
+    addOrder(products: [ID]!): Order
+    updateProduct(_id: ID!, quantity: Int!): Product
 }
 `;
 
